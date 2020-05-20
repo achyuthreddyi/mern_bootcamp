311bca6b2e2f92e1aa4bb340edbcfb8d5bf11bd5
achyuth <achyuth@localhost.localdomain>
2020-05-20 14:13:33 +0530

    neglect this commit

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -1,6 +1,7 @@
 const user = require("../models/user")
 
 exports.getUserById = (req,res,next,id) => {
+    // searching in the database
     user.findById(id).exec((err,user)=>{
         if(err || !user){
             return res.status(400).json({
@@ -9,12 +10,14 @@ exports.getUserById = (req,res,next,id) => {
         }
         req.profile = user
         next()
-    })
+    });
 
 }
 
 exports.getUser = (req,res) =>{
     //TODO get back here for password
+    req.profile.salt = undefined
+    req.profile.encrypt_password = undefined
     return res.json(req.profile)
 
 }
