445d06229bfb0a5c63f089a657839699f0b630af
achyuth <achyuth@localhost.localdomain>
2020-05-23 20:02:22 +0530

    showing all the users on the screen

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -2,6 +2,8 @@ const User = require("../models/user");
 
 
 exports.getUserByID = (req,res,next,id)=>{
+    console.log(id);
+    
     User.findById(id).exec((err,user)=>{
         if(err || !user ){ 
             return res.status(400).json({
@@ -14,7 +16,20 @@ exports.getUserByID = (req,res,next,id)=>{
     })
 
 }
-
+exports.getallUsers = (req,res)=>{
+    User.find().exec((err,user)=>{
+        if(err || !user){
+            console.log(err);
+            
+            return res.status(400).json({
+                error : " no user to throw on to he screen"
+            })
+        }
+        req.profile = user
+        return res.json(req.profile)
+    })
+    
+}
 exports.getUser = (req,res)=> {
     //TODO : get back here for password
     req.profile.salt = undefined
--- projbackend/routes/user.js
+++ projbackend/routes/user.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const router = express.Router()
 
-const {getUserByID,getUser} =  require("../controllers/user")
+const {getUserByID,getUser,getallUsers} =  require("../controllers/user")
 const {isSignedIn,isAuthenticated,isAdmin} =  require("../controllers/authentication")
 
 router.param("userId",getUserByID)
 
 router.get("/user/:userId",isSignedIn,isAuthenticated,getUser)
+
+router.get("/users",getallUsers)
 
 module.exports = router
